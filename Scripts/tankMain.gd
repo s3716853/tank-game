affec38a2659d5c2091feb2be91b1df23d6f69f0
LorcanSmith <lorcanwsmith@gmail.com>
2025-02-20 20:51:08 +0200

    Player can now shoot
    Player can shoot in one of four directions

--- Scripts/tankMain.gd
+++ Scripts/tankMain.gd
@@ -2,22 +2,12 @@ extends Node
 
 var bullet = preload("res://Prefabs/bullet.tscn")
 @export var bullet_spawn = Node2D
-
 #Node handling the tiles which can be moved to
 @export var map = Node2D
-
-func shoot():
-	#Spawning bullet and setting it to shoot from the "BulletSpawn" position
-	var b = bullet.instantiate()
-	add_child(b)
-	b.global_position = bullet_spawn.global_position
-	b.rotation = bullet_spawn.global_rotation
-	
-#THIS WILL GET REPLACED EVENTUALLY, FOR NOW JUST FOR TESTING
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			shoot()
+#Child node containing the arrow buttons to choose which direction to shoot
+@export var arrows : Node2D
+#Turret child, used to set the direction of the turret for shooting
+@export var turret : Node2D
 
 #Hurt Tank
 #Sends signal to child which handles the tank health
@@ -27,7 +17,21 @@ func hurt(amount):
 #Move button pressed, send signal to the tile map to work out which nodes are moveable
 func _on_move_button_pressed() -> void:
 	map.find_moveable_tiles(self.global_position)
-
 ##Will need to be changed to move over time so it looks nicer. Can be done with lerping
 func move(location):
 	self.global_position = location
+
+#Shoot button pressed, send signal to the tile map to work out which direction
+func _on_shoot_button_pressed() -> void:
+	arrows.visible = true
+	
+func shoot(direction):
+	#Set turret direction
+	turret.rotation = direction.angle()
+	
+	#Spawning bullet and setting it to shoot from the "BulletSpawn" position
+	var b = bullet.instantiate()
+	add_child(b)
+	b.global_position = bullet_spawn.global_position
+	b.rotation = bullet_spawn.global_rotation
+	
