244acdb6675b9799ec57f1033d7a5f4e12fb09dc
LorcanSmith <lorcanwsmith@gmail.com>
2025-02-21 07:28:54 +0200

    Update tankMain.gd

--- Scripts/tankMain.gd
+++ Scripts/tankMain.gd
@@ -13,6 +13,15 @@ var max_actions = 2
 #How many current actions remain
 var actions_remaining = 2
 
+#Location where the player should move to
+var target_location = null
+
+var t = 0
+func _process(delta: float) -> void:
+	#Move towards desired location over time
+	if(self.global_position != target_location and target_location != null):
+		t += delta * 0.1
+		self.global_position = self.global_position.lerp(target_location, t)
 #Hurt Tank
 #Sends signal to child which handles the tank health
 func hurt(amount):
@@ -21,10 +30,13 @@ func hurt(amount):
 #Move button pressed, send signal to the tile map to work out which nodes are moveable
 func _on_move_button_pressed() -> void:
 	map.find_moveable_tiles(self.global_position)
-##Will need to be changed to move over time so it looks nicer. Can be done with lerping
+
+#Set move location to node position and then move over time in func _process
 func move(location):
-	self.global_position = location
+	target_location = location
+	t = 0
 	action_taken()
+
 #Shoot button pressed, send signal to the tile map to work out which direction
 func _on_shoot_button_pressed() -> void:
 	arrows.visible = true
