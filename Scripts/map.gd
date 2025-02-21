af0f267835b35fb284d245c45a2835fa1ceaceac
will-ireland <will_ireland@outlook.com>
2025-02-21 16:33:08 +1100

    turned off test output and reset tile visibility

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -178,7 +178,7 @@ func tile_heuristics(player_position):
 			if !child.heuristic:
 				can_move_on = false
 #	test output
-	for child in get_children():
-		child.find_child("Label").text = str(child.heuristic)
-		print(child.location, child.heuristic)
-		child.visible = true
+	#for child in get_children():
+		#child.find_child("Label").text = str(child.heuristic)
+		#print(child.location, child.heuristic)
+		#child.visible = true
